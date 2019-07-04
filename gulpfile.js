2db4a2a054690f77bf52702e212a47d96e2e2160
Dariia Zhyr <darzhyr@gmail.com>
2019-07-04 21:59:11 +0300

    airbnb listing details

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('sass', async function(){
+    return gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// gulp.task('scripts', async function() {
+//     return gulp.src('app/js/main.js')
+//            .pipe(browserSync.reload({ stream: true }));
+// });
+
+// gulp.task('code'), async function() {
+//     return gulp.src('app/*.html')
+//            .pipe(browserSync.reload({ stream: true }));
+// }
+
+gulp.task('browser-sync', async function() {
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'src' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/input.scss', gulp.parallel('sass'));
+    // gulp.watch('src/*.html', gulp.parallel('code'));
+    // gulp.watch('src/main.js', gulp.parallel('scripts'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
